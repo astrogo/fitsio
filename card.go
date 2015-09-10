52a0398eaaacc5b7b15518cac72ba9c2ddfc8b53
Sebastien Binet <binet@cern.ch>
2015-09-10 09:37:57 +0200

    all: add copyright

--- card.go
+++ card.go
@@ -1,3 +1,7 @@
+// Copyright 2015 The astrogo Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package fitsio
 
 type Value interface{}
--- doc.go
+++ doc.go
@@ -1,2 +1,6 @@
+// Copyright 2015 The astrogo Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // fitsio provides read and write access to FITS files.
 package fitsio
