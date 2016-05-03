c65ab132db4b43a5a302f67b96960065c0072f62
Sebastien Binet <binet@cern.ch>
2016-05-03 20:32:59 +0200

    cmd/view-fits: improved performances and features
    This CL improves the performances of the images' display.
    Previously, an image would be redisplayed whenever a paint.Event would
    be received.
    This is wasteful, especially when the window is being manually resized
    or adjusted to the image dimensions.
    
    Instead, only redraw when the user explicitly asks for it (by pressing
    the 'r' key) or when the image window gains focus
    (lifecycle.StageVisible->lifecycle.StageFocused.)
    
    Also, adds the following keybindings:
    - r: reload
    - z: resize/adjust (not yet functional)
    - ?: display help
    - p: print/save current image into/as output.png

--- cmd/view-fits/main.go
+++ cmd/view-fits/main.go
@@ -10,6 +10,7 @@ import (
 	"image"
 	"image/color"
 	"image/draw"
+	"image/png"
 	"log"
 	"os"
 
@@ -18,12 +19,9 @@ import (
 	"golang.org/x/exp/shiny/driver"
 	"golang.org/x/exp/shiny/screen"
 	"golang.org/x/mobile/event/key"
+	"golang.org/x/mobile/event/lifecycle"
 	"golang.org/x/mobile/event/paint"
-)
-
-var (
-	infos []fileInfo
-	help  = flag.Bool("help", false, "show help")
+	"golang.org/x/mobile/event/size"
 )
 
 type fileInfo struct {
@@ -33,23 +31,26 @@ type fileInfo struct {
 
 func main() {
 
-	var r *os.File
-	var f *fitsio.File
-	var err error
+	help := flag.Bool("help", false, "show help")
 
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, `view-fits - a FITS image viewer.
 
 Usage of view-fits:
-$ view-fits [file-names]
+$ view-fits [file1 [file2 [...]]]
 
 Examples:
 $ view-fits astrogo/fitsio/testdata/file-img2-bitpix+08.fits
-$ view-fits astrogo/fitsio/testdata/file-img2-bitpix+08.fits astrogo/fitsio/testdata/file-img2-bitpix+16.fits astrogo/fitsio/testdata/file-img2-bitpix+64.fits
+$ view-fits astrogo/fitsio/testdata/file-img2-bitpix*.fits
 
-Navigation:
-right/left arrows: switch to next/previous file
-up/down arrows: switch to previous/next image in the current file
+Controls:
+- left/right arrows: switch to previous/next file
+- up/down arrows:    switch to previous/next image in the current file
+- r:                 reload/redisplay current image
+- z:                 resize window to fit current image
+- p:                 print current image to 'output.png'
+- ?:                 show help
+- q/ESC:             quit
 `)
 	}
 
@@ -60,18 +61,208 @@ up/down arrows: switch to previous/next image in the current file
 		os.Exit(0)
 	}
 
+	log.SetFlags(0)
+	log.SetPrefix("[view-fits] ")
+
+	infos := processFiles()
+	if len(infos) == 0 {
+		log.Fatal("No image among given FITS files.")
+	}
+
+	type cursor struct {
+		file int
+		img  int
+	}
+
+	driver.Main(func(s screen.Screen) {
+
+		// Number of files.
+		nbFiles := len(infos)
+
+		// Current displayed file and image in file.
+		cur := cursor{file: 0, img: 0}
+
+		// Building the main window.
+		w, err := s.NewWindow(&screen.NewWindowOptions{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer w.Release()
+
+		// Building the screen buffer.
+		b, err := s.NewBuffer(image.Point{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer release(b)
+
+		w.Fill(b.Bounds(), color.Black, draw.Src)
+		w.Publish()
+
+		repaint := true
+		var (
+			sz size.Event
+			//bkg = color.Black
+			bkg = color.RGBA{0xe0, 0xe0, 0xe0, 0xff} // Material Design "Grey 300"
+		)
+
+		for {
+			switch e := w.NextEvent().(type) {
+			default:
+				// ignore
+
+			case lifecycle.Event:
+				switch {
+				case e.From == lifecycle.StageVisible && e.To == lifecycle.StageFocused:
+					repaint = true
+				default:
+					repaint = false
+				}
+				if repaint {
+					w.Send(paint.Event{})
+				}
+
+			case key.Event:
+				switch e.Code {
+				case key.CodeEscape, key.CodeQ:
+					return
+
+				case key.CodeSlash:
+					if e.Direction == key.DirPress && e.Modifiers&key.ModShift != 0 {
+						flag.Usage()
+						continue
+					}
+
+				case key.CodeRightArrow:
+					if e.Direction == key.DirPress {
+						repaint = true
+						if cur.file < nbFiles-1 {
+							cur.file++
+						} else {
+							cur.file = 0
+						}
+						cur.img = 0
+						log.Printf("file:   %v\n", infos[cur.file].Name)
+						log.Printf("images: %d\n", len(infos[cur.file].Images))
+					}
+
+				case key.CodeLeftArrow:
+					if e.Direction == key.DirPress {
+						repaint = true
+						if cur.file == 0 {
+							cur.file = nbFiles - 1
+						} else {
+							cur.file--
+						}
+						cur.img = 0
+						log.Printf("file:   %v\n", infos[cur.file].Name)
+						log.Printf("images: %d\n", len(infos[cur.file].Images))
+					}
+
+				case key.CodeDownArrow:
+					if e.Direction == key.DirPress {
+						repaint = true
+						nbImg := len(infos[cur.file].Images)
+						if cur.img < nbImg-1 {
+							cur.img++
+						} else {
+							cur.img = 0
+						}
+					}
+
+				case key.CodeUpArrow:
+					if e.Direction == key.DirPress {
+						repaint = true
+						nbImg := len(infos[cur.file].Images)
+						if cur.img == 0 {
+							cur.img = nbImg - 1
+						} else {
+							cur.img--
+						}
+					}
+
+				case key.CodeR:
+					if e.Direction == key.DirPress {
+						repaint = true
+					}
+
+				case key.CodeZ:
+					if e.Direction == key.DirPress {
+						// resize to current image
+						// TODO(sbinet)
+						repaint = true
+					}
+
+				case key.CodeP:
+					if e.Direction != key.DirPress {
+						continue
+					}
+					out, err := os.Create("output.png")
+					if err != nil {
+						log.Fatalf("error printing image: %v\n", err)
+					}
+					defer out.Close()
+					err = png.Encode(out, infos[cur.file].Images[cur.img])
+					if err != nil {
+						log.Fatalf("error printing image: %v\n", err)
+					}
+					err = out.Close()
+					if err != nil {
+						log.Fatalf("error printing image: %v\n", err)
+					}
+					log.Printf("printed current image to [%s]\n", out.Name())
+				}
+
+				if repaint {
+					w.Send(paint.Event{})
+				}
+
+			case size.Event:
+				sz = e
+
+			case paint.Event:
+				if !repaint {
+					continue
+				}
+				repaint = false
+				img := infos[cur.file].Images[cur.img]
+
+				release(b)
+				b, err = s.NewBuffer(img.Bounds().Size())
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer release(b)
+
+				draw.Draw(b.RGBA(), b.Bounds(), img, image.Point{}, draw.Src)
+
+				w.Fill(sz.Bounds(), bkg, draw.Src)
+				w.Upload(image.Point{}, b, img.Bounds())
+				w.Publish()
+			}
+
+		}
+
+	})
+}
+
+func processFiles() []fileInfo {
+	infos := make([]fileInfo, 0, len(flag.Args()))
 	// Parsing input files.
 	for _, fname := range flag.Args() {
 
 		finfo := fileInfo{Name: fname}
 
 		// Opening the file.
-		if r, err = os.Open(fname); err != nil {
+		r, err := os.Open(fname)
+		if err != nil {
 			log.Fatal("Can not open the input file: %s.", err)
 		}
+		defer r.Close()
 
 		// Opening the FITS file.
-		if f, err = fitsio.Open(r); err != nil {
+		f, err := fitsio.Open(r)
+		if err != nil {
 			log.Fatal("Can not open the FITS input file: %s.", err)
 		}
 		defer f.Close()
@@ -85,10 +276,8 @@ up/down arrows: switch to previous/next image in the current file
 
 			// Discarding HDU with no axes.
 			if len(axes) != 0 {
-				// Keeping HDU with IMAGE_HDU type.
-				if HDUType := hdu.Type(); HDUType == fitsio.IMAGE_HDU {
-					// Filling the images array.
-					img := hdu.(fitsio.Image).Image()
+				if hdu, ok := hdu.(fitsio.Image); ok {
+					img := hdu.Image()
 					if img != nil {
 						finfo.Images = append(finfo.Images, img)
 					}
@@ -101,109 +290,15 @@ up/down arrows: switch to previous/next image in the current file
 		}
 	}
 
-	if len(infos) == 0 {
-		log.Fatal("No image among given FITS files.")
+	return infos
+}
+
+type releaser interface {
+	Release()
+}
+
+func release(r releaser) {
+	if r != nil {
+		r.Release()
 	}
-
-	driver.Main(func(s screen.Screen) {
-
-		// Number of files.
-		nbFiles := len(infos)
-
-		// Current displayed file and image in file.
-		ifile := 0
-		iimg := 0
-
-		// Building the main window.
-		w, err := s.NewWindow(&screen.NewWindowOptions{})
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer w.Release()
-		// Building the screen buffer.
-		b, err := s.NewBuffer(image.Point{})
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer b.Release()
-
-		w.Fill(b.Bounds(), color.Black, draw.Src)
-		w.Publish()
-
-		for {
-			e := w.NextEvent()
-
-			switch e := e.(type) {
-			default:
-			case key.Event:
-				switch e.Code {
-				case key.CodeEscape, key.CodeQ:
-					return
-				case key.CodeRightArrow:
-					if e.Direction == key.DirPress {
-						if ifile < nbFiles-1 {
-							ifile++
-						} else {
-							ifile = 0
-						}
-						iimg = 0
-						b.Release()
-						w.Send(paint.Event{})
-					}
-				case key.CodeLeftArrow:
-					if e.Direction == key.DirPress {
-						if ifile == 0 {
-							ifile = nbFiles - 1
-						} else {
-							ifile--
-						}
-						iimg = 0
-						b.Release()
-						w.Send(paint.Event{})
-					}
-
-				case key.CodeDownArrow:
-					if e.Direction == key.DirPress {
-						nbImg := len(infos[ifile].Images)
-						if iimg < nbImg-1 {
-							iimg++
-						} else {
-							iimg = 0
-						}
-						b.Release()
-						w.Send(paint.Event{})
-
-					}
-				case key.CodeUpArrow:
-					if e.Direction == key.DirPress {
-						nbImg := len(infos[ifile].Images)
-						if iimg == 0 {
-							iimg = nbImg - 1
-						} else {
-							iimg--
-						}
-						b.Release()
-						w.Send(paint.Event{})
-
-					}
-				}
-
-			case paint.Event:
-				img := infos[ifile].Images[iimg]
-
-				b, err = s.NewBuffer(img.Bounds().Size())
-				if err != nil {
-					log.Fatal(err)
-				}
-				defer b.Release()
-
-				draw.Draw(b.RGBA(), b.Bounds(), img, image.Point{}, draw.Src)
-
-				w.Upload(image.Point{}, b, img.Bounds())
-				w.Publish()
-			}
-
-		}
-
-	})
 }
